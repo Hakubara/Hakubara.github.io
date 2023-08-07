ec4d5d0403d3d9ba5d6290f2c213617a227a9a62
Hakubara <1925181138@qq.com>
2023-08-08 00:33:14 +0800

    Site updated: 2023-08-08 00:33:14

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/08/06/hello-world/","2023/08/07/test/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/08/07/test/","2023/08/06/hello-world/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
